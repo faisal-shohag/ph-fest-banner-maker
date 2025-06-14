6a2240da4c962da32ff41b637110463d71dc27dc
faisal-shohag <faisalshohagprog@gmail.com>
2025-06-14 09:11:06 +0600

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { router } from './routes.tsx'
 import { AuthProvider } from './contexts-providers/auth-provider.tsx'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/react-query.ts'
+import { Toaster } from 'react-hot-toast'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
     <ThemeProvider>
     <AuthProvider>
       <RouterProvider router={router} />
+            <Toaster/>
     </AuthProvider>
     </ThemeProvider>
     </QueryClientProvider>
